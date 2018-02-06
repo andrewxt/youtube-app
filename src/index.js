d6dc5fe3b32c3756bf7ecdcc738843b9f4d08278
Andrew C <andrew@Andrews-MacBook-Pro.local>
2018-02-06 15:22:49 -0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAB2jW_LHqgJzcGzVVQpXWm3ARahcYExf8';
+
 // Create new component. This component sould produce some HTML
+const App = () => { 
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+}
 
 // Take this compnent's generated HTML and put it in the DOM
-
+ReactDOM.render(<App />, document.querySelector('.container'));
